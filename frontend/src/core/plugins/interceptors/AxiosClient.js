e0524a7255def71ea1376af7c6ef03718acfef13
Md Azharuddin <stfuazzo@gmail.com>
2024-07-20 22:54:04 +0530

    Update AxiosClient.js

--- frontend/src/core/plugins/interceptors/AxiosClient.js
+++ frontend/src/core/plugins/interceptors/AxiosClient.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import {} from "dotenv/config";
 
 const config = {
+  baseURL: " https://mindcirclebackend.onrender.com/",
   timeout: process.env.TIMEOUT,
   // headers: { "Content-Type": "application/json" },
   // withCredentials: true,
@@ -12,5 +13,4 @@ const axiosClient = () => {
     ...config,
   });
 };
-
 export { axiosClient };
